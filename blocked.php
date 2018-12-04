7008c9cca2202c030806b19550d4531a702b7568
Xioann02 <xioann02@cs.ucy.ac.cy>
2018-12-04 23:11:30 +0200

    🚀🚀

---
+++ blocked.php
@@ -0,0 +1,11 @@
+<html class="login-page">
+  <head>
+    <link rel="stylesheet" href="style.css"/>
+  </head>
+  <body>
+    <div class="block-box">
+      <div class="block-red">Access denied!</div>
+      <div class="block-info">Max number of attempts reached.</div>
+    </div>
+  </body>
+</html>
